1176416a479a9e60cf5bc327203aa82e12dcc903
Okada <ringoapple.sukebasuke@gmail.com>
2023-03-01 17:09:16 +0900

    全体的にスタイルを合わせる

--- JS/common.js
+++ JS/common.js
@@ -1,22 +1,45 @@
 $(function () {
-  $(".slider").slick({
-    autoplay: true,
-    dots: true,
-    arrows: true,
-    adaptiveHeight: true,
-    centerMode: true,
-    slidesToShow: 2,
-    slidesToScroll: 1,
-    centerPadding: "15%"
-  });
+  let width = $(window).width();
+  if(width >= 750) {
+    $(".slider").slick({
+      autoplay: true,
+      dots: true,
+      arrows: true,
+      adaptiveHeight: true,
+      centerMode: true,
+      slidesToShow: 2,
+      slidesToScroll: 1,
+      centerPadding: "15%"
+    });
+  } else {
+    $(".slider").slick({
+      autoplay: true,
+      arrows: false,
+      dots: true,
+      adaptiveHeight: true,
+      slidesToShow: 1,
+      slidesToScroll: 1
+    });
+  }
 });
 
 $(function () {
-  $(".slider_recommend").slick({
-    autoplay: true,
-    arrows: true,
-    adaptiveHeight: true,
-    slidesToShow: 6,
-    slidesToScroll: 1
-  });
+  let width = $(window).width();
+  if(width >= 750) {
+    $(".slider_recommend").slick({
+      autoplay: true,
+      arrows: true,
+      adaptiveHeight: true,
+      slidesToShow: 6,
+      slidesToScroll: 1
+    });
+  } else {
+    $(".slider_recommend").slick({
+      autoplay: true,
+      arrows: false,
+      adaptiveHeight: true,
+      slidesToShow: 2,
+      slidesToScroll: 1
+    });
+  }
 });
